57e758336b79b94229d0570f0b57f4844f2d71f8
Andrej <a.user.de@gmail.com>
2021-03-12 16:52:32 +0100

    add loader

--- vof-checker.js
+++ vof-checker.js
@@ -1,18 +1,39 @@
 const template = document.createElement('template')
+let loading = false, checkedDown = false, hasError = false
 template.innerHTML = `
-    <style>
-       
-    </style>
-    <div id="vof-checker__root">
-        <form id="vof-checker__form">
-            <label for="vof-checker__input" id="vof-checker__label"></label>
-            <input id=""vof-checker__input" type="text" />
-            <button id="vof-checker__button"></button>
+<style>
+
+.loader,.loader:before,.loader:after{
+    display: none;
+    box-sizing: border-box;
+    flex-grow: 0;
+    flex-shrink: 0;
+}
+.loader.awesome-spin {
+    border-radius: 50%;
+    color: var(--bs-primary, #33f);
+    border-top: solid 2px;
+    border-bottom: solid 2px;
+    width: 20px;
+    height: 20px;
+    animation: awesome-spin 2s  linear infinite;
+  }
+  
+  @keyframes awesome-spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+</style>
+    <div id="vof-checker__root" class="vof-checker__root">
+        <form id="vof-checker__form" class="vof-checker__form">
+            <label for="vof-checker__input" id="vof-checker__label" class="vof-checker__label"></label>
+            <input id="vof-checker__input" class="vof-checker__input" type="text" />
+            <button id="vof-checker__button" class="vof-checker__button"></button>
         </form>
-        <div id="vof-checker__feedback"></div>
+        <div id="vof-checker__feedback" class="vof-checker__feedback"></div>
+        <div class="loader awesome-spin"></div>
     </div>
 `
-
 class VofChecker extends HTMLElement {
     constructor(){
         super()
@@ -21,27 +42,41 @@ class VofChecker extends HTMLElement {
         this.shadowRoot.querySelector('#vof-checker__button').innerText = this.getAttribute('btnText')
         this.shadowRoot.querySelector('#vof-checker__label').innerText = this.getAttribute('labelText')
         this.shadowRoot.querySelector('#vof-checker__feedback').innerText = this.getAttribute('feedbackText')
-
-        this.getAttribute('rootClass') ?? this.shadowRoot.querySelector('#vof-checker__root').setAttribute('class', this.getAttribute('rootClass'))
-        this.getAttribute('formClass') ?? this.shadowRoot.querySelector('#vof-checker__form').setAttribute('class', this.getAttribute('formClass'))
-        this.getAttribute('inputClass') ?? this.shadowRoot.querySelector('#vof-checker__input').setAttribute('class', this.getAttribute('inputClass'))
-        this.getAttribute('btnClass') ?? this.shadowRoot.querySelector('#vof-checker__button').setAttribute('class', this.getAttribute('btnClass'))
-        
+        loading
+          ? (this.shadowRoot.querySelector('.loader').style.display =
+              'inline-flex')
+          : (this.shadowRoot.querySelector('.loader').style.display =
+              'none')
     }
 
     async checkDomain(value){
+        loading = true
+        const isRaMicro = this.getAttribute('isRaMicro') === "ja"
+        const partnerId = this.getAttribute('partnerId') !== ""
+
         const headers = new Headers()
         headers.append('Content-Type', 'application/json')
         headers.append('Accept', 'application/json')
+        
         const resData = await fetch(
-          `https://${value}${'.voffice.pro'}/api/namespaceExists`,
+          `https://${value}${
+              isRaMicro ?
+              '.ra-micro.voffice.pro' :
+              '.voffice.pro'
+            }/api/namespaceExists`,
           {
             headers: headers,
           }
         )
           .then((data) => data.json())
-          .then((res) => console.log(res))
+          .then((res) => {
+              loading = false
+              checkedDown = true
+              console.log(res)
+          })
           .catch((error) => {
+              hasError = true
+              loading = false
             return console.error('Error', error)
           })
     }
